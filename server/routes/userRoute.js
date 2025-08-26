1b11e29c3470fe49d04dff54f82629b12e14c3ab
vatsal023 <vatsalgarg504@gmail.com>
2025-08-26 23:59:13 +0530

    feat(profile) :  see user profile

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const registerController = require("../Controllers/registerController");
 const loginController = require("../Controllers/loginController");
+const peopleController = require("../Controllers/peopleController");
+const verifyEmail = require("../Controllers/emailverificationController");
+const { profileController,profileUpdate } = require("../Controllers/profileController");
 
 const router = express.Router();
 
 router.post('/register',registerController);
 router.post('/login',loginController);
+router.get('/:id/verify/:token',verifyEmail);
+router.get('/people',peopleController);
+router.get('/profile',profileController);
+router.get('/profile/update',profileUpdate);
+
 
 module.exports = router;
