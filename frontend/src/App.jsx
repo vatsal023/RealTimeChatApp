7b5be115d98a676be7a22e060f27cf8a29c53721
vatsal023 <vatsalgarg504@gmail.com>
2025-09-10 22:55:52 +0530

    feat(register): made the register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Register from './pages/register';
+// import Register from './pages/Register.jsx'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Register1 from './pages/Register1';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+   const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<><h1>Helloworld</h1></>
+    },
+    {
+      path:"/register",
+      element:<><Register/></>
+    },
+    {
+      path:"/register1",
+      element:<><Register1/></>
+    }
+   ])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className = "bg-red-600">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <RouterProvider router = {router}/>
+
     </>
   )
 }
