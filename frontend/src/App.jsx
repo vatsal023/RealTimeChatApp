41093b8180c859be96c0e0038006bc092ff5d3a5
vatsal023 <vatsalgarg504@gmail.com>
2025-09-13 13:13:47 +0530

    feat: update App component logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import './App.css'
-import Register from './pages/register';
-// import Register from './pages/Register.jsx'
+import Register from './pages/Register';
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import Register1 from './pages/Register1';
 import Login from './pages/Login';
+import VerifyEmail from './pages/verifyEmail';
+import { AuthProvider, useAuth } from "./context/authContext";
+import {ProfileProvider} from "./context/profileContext"
+import LandingNav from './components/LandingNav';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Payments from './components/Payment';
+import Footer from './components/Footer';
+import CustomerLogos from './components/CustomerLogos';
+import Home from './pages/Home';
 
 function App() {
   
@@ -20,13 +29,48 @@ function App() {
     {
       path:"/login",
       element:<><Login/></>
+    },
+    {
+      path:"/users",
+      element:<><VerifyEmail/></>
+    },
+    {
+      path:"/landing",
+      element:<><LandingNav/></>
+    },
+    {
+      path:"/hero",
+      element:<><Hero/></>
+    },
+    {
+      path:"/features",
+      element:<><Features/></>
+    },
+    {
+      path:"/payment",
+      element:<><Payments/></>
+    },
+    {
+      path:"/footer",
+      element:<><Footer/></>
+    },
+    {
+      path:"/logos",
+      element:<CustomerLogos/>
+    },
+    {
+      path:"/home",
+      element:<Home/>
     }
    ])
 
   return (
     <>
-       <RouterProvider router = {router}/>
-
+    <AuthProvider>
+      <ProfileProvider>
+        <RouterProvider router = {router}/>
+      </ProfileProvider>
+    </AuthProvider>
     </>
   )
 }
