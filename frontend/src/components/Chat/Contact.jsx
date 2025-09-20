a8dad0caae4d295cc6af95afbf15633e94fd3f79
vatsal023 <vatsalgarg504@gmail.com>
2025-09-20 22:39:07 +0530

    feat(ui): create contact component

---
+++ frontend/src/components/Chat/Contact.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+// import Avatar from "./Avatar"
+
+const Contact = ({ userId, username, selectedUserId, setSelectedUserId, isOnline }) => {
+    return (
+        <li
+            key={userId}
+            className={`${selectedUserId === userId ? "bg-primary" : ""
+                } capitalize py-2 lg:py-3 px-2 lg:px-5  rounded-[1.3rem]  border-gray-300 hover:bg-primary flex flex-col lg:flex-row items-center gap-1 my-1.5 lg:gap-4 font-medium hover:cursor-pointer lg:my-3 text-white `}
+            onClick={() => {
+                setSelectedUserId(userId);
+                console.log(userId)
+            }}
+        >
+            <span className="text-xs lg:text-base text-center">{username}</span>
+            {isOnline && (
+                <span
+                    className={`text-xs rounded-full bg-green-500 px-2 py-0.5  z-20 `}
+                >
+                    Active
+                </span>
+            )}
+        </li>
+    )
+}
+
+export default Contact;
