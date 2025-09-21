846534c4dab857be33b8eba24f790a593fe659a4
vatsal023 <vatsalgarg504@gmail.com>
2025-09-21 23:38:32 +0530

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import Register from './pages/Register';
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet, ScrollRestoration, } from 'react-router-dom'
 import Register1 from './pages/Register1';
 import Login from './pages/Login';
 import VerifyEmail from './pages/verifyEmail';
 import { Toaster } from "react-hot-toast";
 import { AuthProvider, useAuth } from "./context/authContext";
-import {ProfileProvider} from "./context/profileContext"
-import LandingNav from './components/LandingNav';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import Payments from './components/Payment';
-import Footer from './components/Footer';
-import CustomerLogos from './components/CustomerLogos';
+import { ProfileProvider } from "./context/profileContext"
 import Home from './pages/Home';
 import ChatMessages from './components/Chat/ChatMessages';
 import Nav from './components/Chat/Nav';
@@ -21,64 +15,76 @@ import TopBar from './components/Chat/TopBar';
 import ChatHome from './pages/ChatHome';
 import Profile from './components/Profile';
 import ChatHomeMock from './pages/ChatHome1';
+import axios from "axios";
+import { baseUrl } from '../apiconfig';
 
-function App() {
-  
-   const router = createBrowserRouter([
-    {
-      path:"/",
-      element:<h1>Helloworld</h1>
-    },
-    {
-      path:"/register",
-      element:<Register/>
-    },
-    {
-      path:"/login",
-      element:<Login/>
-    },
-    {
-      path:"/users",
-      element:<VerifyEmail/>
-    },
-    {
-      path:"/home",
-      element:<Home/>
-    },
-    {
-      path:"/msg",
-      element:<ChatMessages/>
-    },
-    {
-      path:"/nav",
-      element:<Nav/>
-    },
-    {
-      path:"/top",
-      element:<TopBar/>
-    },
-    // {
-    //   path:"/chathome",
-    //   element:<ChatHome/>
-    // },
-    {
-      path:"/chathome",
-      element:<ChatHomeMock/>
-    },
-    {
-      path:"/profile",
-      element:<Profile/>
-    }
-   ])
+const Layout = () => {
+  const { isAuthenticated, checkAuth } = useAuth();
+
+  useEffect(() => {
+    checkAuth();
+  }, [isAuthenticated]);
 
   return (
     <>
-    <AuthProvider>
-      <ProfileProvider>
-        <RouterProvider router = {router}/>
-        <Toaster/>
-      </ProfileProvider>
-    </AuthProvider>
+      <ScrollRestoration />
+      <Outlet />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "register",
+        element: <Register />
+      },
+      {
+        path: "login",
+        element: <Login />
+      },
+      {
+        path: "users/:id/verify/:token",
+        element: <VerifyEmail />
+      },
+      {
+        path:"chathome",
+        element:<ChatHome/>
+      },
+      {
+        path: "chathome1",
+        element: <ChatHomeMock />
+      },
+      {
+        path: "profile",
+        element: <Profile />
+      }
+    ]
+  }
+])
+
+function App() {
+
+
+  axios.defaults.baseURL = baseUrl;
+  axios.defaults.withCredentials = true;
+
+
+  return (
+    <>
+      <AuthProvider>
+        <ProfileProvider>
+          <RouterProvider router={router} />
+          <Toaster />
+        </ProfileProvider>
+      </AuthProvider>
     </>
   )
 }
