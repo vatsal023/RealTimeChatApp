8c014c33eefa539e34b7e0868fbe0d32f2619c92
vatsal023 <vatsalgarg504@gmail.com>
2025-09-21 23:42:21 +0530

    chore: add apiconfig for API and socket URLs

---
+++ frontend/apiconfig.js
@@ -0,0 +1,12 @@
+let baseUrl;
+let socketUrl;
+
+if(import.meta.env.VITE_NODE_ENV==="production"){
+    baseUrl = "your-deployed-URL";
+    socketUrl = "wss://your-deployed-url";
+}else{
+    baseUrl = "http://localhost:8001"
+    socketUrl = "ws://localhost:8001";
+}
+
+export {baseUrl,socketUrl};
