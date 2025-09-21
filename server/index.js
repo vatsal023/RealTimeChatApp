60a25fb50a021ccd13f782658c4f9da1d567df78
vatsal023 <vatsalgarg504@gmail.com>
2025-09-21 23:40:11 +0530

    update index.js for server

--- server/index.js
+++ server/index.js
@@ -1,28 +1,53 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require('cookie-parser')
 const connection  = require('./db/db.js')
+const createWebSocketServer = require("./wsServer.js");
+const path = require("path");
 
 const userRoute = require("./routes/userRoute.js")
 
 const app = express();
-const PORT = 8001;
+// const PORT = 8001;
 
-app.use(cookieParser());
 //database connection
 connection();
 
 //middlewares
+app.use(cookieParser());
 app.use(express.json());
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:4000",
+    "https://swifty-chatty-appy.onrender.com"
+];
+
+const corsOptions = {
+    origin :(origin,callback) =>{
+        if(allowedOrigins.includes(origin)|| !origin){
+            callback(null,true);
+        }else{
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus :204,
+    credentials: true,
+}
+app.use(cors(corsOptions));
+
 app.use("/api/user",userRoute)
-// app.use(cookieParser());
+// app.get("/use",(req,res)=>{
+//     res.send("Hello world")
+// })
 
-app.get("/use",(req,res)=>{
-    res.send("Hello world")
-})
+const PORT = 8001;
+// const PORT = process.env.PORT||8000;
 
-
-
-app.listen(PORT,()=>{
+const server = app.listen(PORT,()=>{
     console.log(`Server started at PORT ${PORT} `)
 })
+
+createWebSocketServer(server);
