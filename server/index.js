92aa59842ea8a397d9d607048dc295122f3dce3a
vatsal023 <vatsalgarg504@gmail.com>
2025-09-27 23:38:26 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -51,3 +51,12 @@ const server = app.listen(PORT,()=>{
 })
 
 createWebSocketServer(server);
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend","dist","index.html"), (err) => {
+//         if (err) {
+//             console.error('Error sending file:', err);
+//         }
+//     });
+// });
